b6e33876454f3c5d19e5d005d1f98b54ef4abdc8
ofirc298 <ofirc298@gmail.com>
2022-12-28 12:51:30 +0200

    finish server

--- server.js
+++ server.js
@@ -1,12 +1,44 @@
 const express = require('express')
 const { buildSchema } = require('graphql')
 const { graphqlHTTP } = require('express-graphql')
+const axios = require('axios')
 
 const app = express();
 
+let message = "this is a message";
+
 const schema = buildSchema(`
+    type Post {
+        userId:Int
+        id:Int
+        title:String
+        body:String
+    }
+
+    type User {
+        name:String
+        age:Int
+        college:String
+    }
+
     type Query {
-        hello:String
+        hello:String!
+        welcomeMessage(name: String, dayOfweek: String!): String
+        getUser:User
+        getUsers:[User]
+        getPostsFromExternalAPI:[Post]
+        message:String
+    }
+
+    input UserInput {
+        name:String!
+        age:Int!
+        college:String!
+    }
+
+    type Mutation {
+        setMessage(newMessage:String):String
+        createUser(user: UserInput): User
     }
 `)
 
@@ -14,6 +46,46 @@ const root = {
     hello: () => {
         return "hello world!";
     },
+    welcomeMessage: (args) => {
+        console.log(args);
+        return `Hello ${args.name}, hows life, today is ${args.dayOfweek}`;
+    },
+    getUser: () => {
+        const user = {
+            name: "ofir",
+            age: "32",
+            college: "ness",
+        }
+
+        return user;
+    },
+    getUsers: () => {
+        const users = [{
+            name: "ofir",
+            age: "32",
+            college: "ness",
+        },
+        {
+            name: "dan",
+            age: "30",
+            college: "ruppin",
+        }]
+
+        return users;
+    },
+    getPostsFromExternalAPI: async () => {
+        const result = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        return result.data;
+    },
+    setMessage: ({ newMessage }) => {
+        message = newMessage;
+        return message
+    },
+    message: () => message,
+    createUser: (args) => {
+        console.log(args)
+        return args.user;
+    }
 }
 
 app.use('/graphql',
